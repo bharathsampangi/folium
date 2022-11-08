49584b155944440ef549d516965b3bacde79dcf1
bharath-spurtree <bharath.s@spurtreetech.in>
2022-11-08 11:48:36 +0530

    completed footer

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -3,14 +3,41 @@ import styles from "./styles/Footer.module.css"
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <div>
-                
+            <div className={styles.footerOptions}>
+                <div className={styles.footerOption1}>
+                    <div className={styles.head}>
+                        +91 9066392322<br/>
+                        bharathsampangi95@gmail.com
+                    </div>
+                    <div className={styles.text}>
+                        Borewell Rd, Whitefield,<br/>
+                        Bengaluru, 560066
+                    </div>
+                    <button className={styles.button}>download brochure</button>
+                </div>
+                <div className={styles.footerOption2}>
+                    <div className={styles.head}>Menu</div>
+                    <ul className={styles.list}>
+                        <li>enquire now</li>
+                        <li>location</li>
+                        <li>community</li>
+                        <li>types of homes</li>
+                        <li>about sumadhura</li>
+                    </ul>
+                </div>
+                <div className={styles.footerOption3}>
+                    <div className={styles.head}>Navigation</div>
+                    <ul className={styles.list}>
+                        <li>project</li>
+                        <li>features</li>
+                        <li>details</li>
+                    </ul>
+                </div>
             </div>
-            <div>
-
-            </div>
-            <div>
-
+            <div className={styles.line}></div>
+            <div className={styles.footerEnd}>
+                <div></div>
+                <div>Terms & Conditions</div>
             </div>
         </footer>
     )
